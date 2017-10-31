7620d722c8fe22ac0a7482b99e5951bc8151d2b0
wathanyu1987 <33229614+wathanyu1987@users.noreply.github.com>
2017-10-31 16:48:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "เบอร์อะไร";
-}else if($arrJson['events'][0]['message']['text'] == DNI-102){
+}else if($arrJson['events'][0]['message']['text'] == "DNI-102"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
