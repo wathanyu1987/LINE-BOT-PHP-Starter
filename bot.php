4bf0465327a41133943c02202d636a099b953b57
wathanyu1987 <33229614+wathanyu1987@users.noreply.github.com>
2017-10-31 12:06:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,6 +21,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+ }else if($arrJson['events'][0]['message']['text'] == "เช็คของ"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "เบอร์อะไร";
 }else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
